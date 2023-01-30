3170372aa9a2ed01b7804bd2b57b739b45c8f6ac
Nimmadev <nimma.singh@outlook.com>
2023-01-30 10:03:45 +0530

    makepubluic

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 import setuptools
 
-import pypandoc
-pypandoc.download_pandoc()
-long_description = pypandoc.convert_file('README.md', 'rst')
 
 setuptools.setup(
     name="pyroadd",
     version="0.0.4",
     autor="nimmadev",
     description="module to add members to telegam group",
-    long_description=long_description,
+    long_description="README at : https://github.com/nimmadev/pyroadd",
     pakages=["pyrogram", "tgcrypto", "readchar"]
 )
